ef1fc3b3932333dbe249d384d0ce18216c926628
Dan <dan@aredherring.tech>
2022-05-07 03:04:41 +0100

    Add example of displaying threats

--- Main.lua
+++ Main.lua
@@ -21,8 +21,55 @@ function DungeonMobSummaryController__OnTargetChanged(self, targetFrame, event, 
 	print("IMPLEMENT ME: Update text to show info for mob!")
 end
 
-
 local addonFrame = CreateFrame("Frame", "DungeonMobSummaryController")
 addonFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 addonFrame:SetScript("OnEvent", DungeonMobSummaryController__OnEvent)
 
+-- These are not bit flags.
+-- The numbers are used so we can refer to each threat by its key in code.
+local Threats = {
+	TankMustStayInRange = 0x01,
+	TankBuster = 0x02,
+	Interrupt = 0x03,
+	Decurse = 0x04,
+	Purge = 0x05,
+	Disease = 0x06,
+	Poison = 0x07,
+	Bleed = 0x08
+}
+
+local ThreatTranslations = {
+	[Threats.TankMustStayInRange] = "Stay in range",
+	[Threats.TankBuster] = "Tank Buster",
+	[Threats.Poison] = "Applies poison",
+	[Threats.Disease] = "Applies disease"
+}
+
+local AbilityThreatList = {
+	-- Doctor Ickus
+	[164967] = {
+		-- Burning Strain
+		[322358] = {Threats.TankMustStayInRange},
+		-- TODO: Dummy threat
+		[1234] = {Threats.TankBuster, Threats.Poison, Threats.Disease}
+	}
+}
+
+function CreateThreatDescription(abilities)
+	local uniqueThreats = {}
+	for _, threats in pairs(abilities) do
+		for _, v in pairs(threats) do
+			uniqueThreats[v] = true
+		end
+	end
+
+	local components = {}
+	for threat, _true in pairs(uniqueThreats) do
+		table.insert(components, ThreatTranslations[threat])
+	end
+
+	return table.concat(components, "·")
+end
+
+local td = CreateThreatDescription(AbilityThreatList[164967])
+targetFrameTextFrame.text:SetText(td)
